8a70f28ff94ebbeb404aef0f0eb89ac6882d0377
bank <bank@bank-laptop.(none)>
2014-04-18 21:27:25 +0700

    Added comments to GlowingIron_IndexedImage.java (Ex 12.7)

--- src/GlowingIron_IndexedImage.java
+++ src/GlowingIron_IndexedImage.java
@@ -10,11 +10,12 @@ public class GlowingIron_IndexedImage implements PlugInFilter {
 	private static final int STEP = 2;
 	@Override
 	public void run(ImageProcessor ip) {
-		byte[] r = new byte[SIZE];
-		byte[] g = new byte[SIZE];
-		byte[] b = new byte[SIZE];
+		byte[] r = new byte[SIZE]; //red
+		byte[] g = new byte[SIZE]; //green
+		byte[] b = new byte[SIZE]; //blue
 		
-		int i;
+		int i; //The index in the lookup table
+		//From dark red (black) to really glowing red
 		byte value = 0;
 		for(i=0; i<=84; i++){
 			r[i] = value;
@@ -22,6 +23,7 @@ public class GlowingIron_IndexedImage implements PlugInFilter {
 			b[i] = 0;
 			value += STEP;
 		}
+		//From glowing red to glowing yellow
 		value = 0;
 		for(i=85; i<=169; i++){
 			r[i] = (byte)255;
@@ -29,6 +31,7 @@ public class GlowingIron_IndexedImage implements PlugInFilter {
 			b[i] = 0;
 			value += STEP;
 		}
+		//From glowing yellow to white
 		value = 0;
 		for(i=170; i<=254; i++){
 			r[i] = (byte)255;
@@ -42,6 +45,7 @@ public class GlowingIron_IndexedImage implements PlugInFilter {
 		
 		LUT lut = new LUT(r, g, b);
 		
+		//Set the lookup table for the image
 		ip.setLut(lut);
 		
 		imp.updateAndDraw();
